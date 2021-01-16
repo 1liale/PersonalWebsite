792fc88cc9b05ec0ec6f1ca4b4e1881b0bdc3684
1liale <1liale@hdsb.ca>
2021-01-15 19:28:57 -0500

    Major Code Cleanup + Quote API

--- gallery.js
+++ gallery.js
@@ -1,48 +1,24 @@
-function loadGallery(){
-    var curItem = '';
+var curItem = '';
+
+function loadModal(i) {
     var itemsList1 = ['cell1','cell2','cell3','cell4','cell5'];
     var itemsList2 = ['#gallery-pop1','#gallery-pop2','#gallery-pop3','#gallery-pop4','#gallery-pop5'];
 
-    document.getElementById(itemsList1[0]).addEventListener("click",
-        function()
-        {
-            document.querySelector('.bg-modal').style.display = 'flex';
-            document.querySelector(itemsList2[0]).style.visibility = 'visible';
-            curItem = itemsList2[0];
-        });
+    document.getElementById(itemsList1[i]).addEventListener("click",
+            function()
+            {
+                document.querySelector('.bg-modal').style.display = 'flex';
+                document.querySelector(itemsList2[i]).style.visibility = 'visible';
+                curItem = itemsList2[i];
+            });
+}
 
-    document.getElementById(itemsList1[1]).addEventListener("click",
-        function()
-        {
-            document.querySelector('.bg-modal').style.display = 'flex';
-            document.querySelector(itemsList2[1]).style.visibility = 'visible';
-            curItem = itemsList2[1];
-        });
+function loadGallery(size){
 
-
-    document.getElementById(itemsList1[2]).addEventListener("click",
-        function()
-        {
-            document.querySelector('.bg-modal').style.display = 'flex';
-            document.querySelector(itemsList2[2]).style.visibility = 'visible';
-            curItem = itemsList2[2];
-        });
-
-    document.getElementById(itemsList1[3]).addEventListener("click",
-        function()
-        {
-            document.querySelector('.bg-modal').style.display = 'flex';
-            document.querySelector(itemsList2[3]).style.visibility = 'visible';
-            curItem = itemsList2[3];
-        });
-
-    document.getElementById(itemsList1[4]).addEventListener("click",
-        function()
-        {
-            document.querySelector('.bg-modal').style.display = 'flex';
-            document.querySelector(itemsList2[4]).style.visibility = 'visible';
-            curItem = itemsList2[4];
-        });
+    for (var i = 0; i < size; i++)
+    {
+        loadModal(i);
+    }
 
     document.querySelector('.close').addEventListener("click", 
     function() {
