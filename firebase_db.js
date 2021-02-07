f7f11d23202c44bf0a2485fe4bb60385427eb4f9
1liale <1liale@hdsb.ca>
2021-02-07 15:36:30 -0500

    leave site bug fixed

--- firebase_db.js
+++ firebase_db.js
@@ -114,7 +114,7 @@
         // Cancel the event
         //e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
         // Chrome requires returnValue to be set
-        e.returnValue = 'quit';
+        // e.returnValue = 'quit';
     });
 
 }());
